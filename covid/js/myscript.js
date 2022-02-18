468cf5487f0078ec0cd730b116a1d37c975b18b3
1904kishan <kishanpatel22771@gmail.com>
2022-02-18 21:53:05 +0530

    covid

---
+++ covid/js/myscript.js
@@ -0,0 +1,44 @@
+const URL = "https://covid19.mathdro.id/api/";
+let app = angular.module("MyApp", []);
+app.controller("MyCtrl", ($scope,$http) =>
+{
+     $scope.title = "Stay Home Stay Safe";
+     console.log("app loaded");
+     //calling app
+     $http.get(URL).then
+     (
+        (response)=>
+        {
+            console.log(response);
+            $scope.all_data=response.data;
+        },
+        (error)=>
+        {
+            console.log(errors);
+        }
+       );
+        //get country
+
+
+        $scope.get_c_data = () =>
+         {
+            let countries=$scope.c;
+            if(countries=="")
+            {
+                return;
+            }
+            $http.get('${URL}/countries/${countries}').then
+             (
+                 (response)=>
+             {
+                 console.log(response.data);
+                 $scope.c_data = response.data;
+             },
+             (error)=>
+             {
+                 console.log(errors);
+             }
+            )
+         };  
+          
+});
